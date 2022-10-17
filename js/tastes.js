90e4a23dfbbf5e62d417f1cdda0debf751aeac22
sergey-maslov <shift.tfihs@gmail.com>
2022-10-17 17:56:33 +0000

    tastes carousel object added

---
+++ js/tastes.js
@@ -0,0 +1,32 @@
+const carousel = [
+    {
+        img: "img/balls1.png",
+        title: "Strawberry Gem",
+        subtitle: "fruity and fresh",
+        price: "$5",
+    },
+    {
+        img: "img/balls2.png",
+        title: "Choco & Hazelnut",
+        subtitle: "rich sweet taste",
+        price: "$5",
+    },
+    {
+        img: "img/balls3.png",
+        title: "Blue Paradise",
+        subtitle: "sour and fruity",
+        price: "$5",
+    },
+    {
+        img: "img/balls4.png",
+        title: "Mango Dreams",
+        subtitle: "classic tropic taste",
+        price: "$5",
+    },
+    {
+        img: "img/balls5.png",
+        title: "Pistachio HHeaven",
+        subtitle: "creamy nutty flavor",
+        price: "$5",
+    },
+]
